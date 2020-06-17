21cd43fcd00b8816c296c7ec92680052a55f878e
eerki <eerik2000@hotmail.com>
2020-06-17 16:05:55 +0300

    test

---
+++ delete-user.php
@@ -0,0 +1,37 @@
+<?php
+require_once("connection.php");
+
+$action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+
+if (isset($action) && $action == 'new-user') {
+
+    $error = [];
+
+    if (empty($error)) {
+        $sql = "DELETE FROM todo_users WHERE id= //current user_id";
+
+        if ($mysqli->query($sql) === TRUE) {
+            echo "New record deleted successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $mysqli->error;
+        }
+
+        $mysqli->close();
+    }
+}
+
+?>
+<h5>Add new User <a href="#">back</a></h5>
+
+<form method="post">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+               placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button name="action" value="new-user" type="submit" class="btn btn-primary">Create</button>
+</form>
